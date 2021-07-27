e878a10ae6cf630aefe0a6b2da0b40d79f290c93
camiloxzc <corozco083@misena.edu.co>
2021-07-27 18:37:47 -0500

    Cambio pruebas master Point

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ use App\Http\Controllers\ServicioProductoController;
 Route::get('/', function () {
     return view('welcome2');
 });
-
+/*modulo producto*/
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/ServiciosProductos/create', [App\Http\Controllers\ServicioProductoController::class, 'create']);
