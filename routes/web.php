96478a41cb931578512f36b08a947f203467fcc7
sugemeza <69494263+sugemeza@users.noreply.github.com>
2021-07-27 18:20:59 -0500

    Duplicado

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use App\Http\Controllers\ServicioProductoController;
 Route::get('/', function () {
     return view('welcome2');
 });
-
+Route::get('/', function () {
+    return view('welcome2');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/ServiciosProductos/create', [App\Http\Controllers\ServicioProductoController::class, 'create']);
