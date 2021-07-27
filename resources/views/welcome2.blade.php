cdd8df073b74903791801ff9266b694b1ae0aed5
Camilo O <coarenas1999@gmail.com>
2021-07-27 11:13:49 -0500

    Cambios pantalla de bienvenida

---
+++ resources/views/welcome2.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main', ['class' => 'off-canvas-sidebar','activePage' => 'dashboard', 'title' => __('')])
+
+@section('content')
+<div class="container" style="height: auto;">
+  
+</div>
+@endsection
+
